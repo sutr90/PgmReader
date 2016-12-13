cb1fe496a314e9132255cf023d649ec1944e510d
fricv <fricv@kiv.zcu.cz>
2016-12-13 15:37:03 +0100

    added comment.

--- filtering.c
+++ filtering.c
@@ -2,12 +2,20 @@
 #include <stdio.h>
 #include <math.h>
 
+
+/*
+ * Sobeluv filtr pro smer X.
+ */
 int sobelXKernel[3][3] = {
         {-1, 0, 1},
         {-2, 0, 2},
         {-1, 0, 1}
 };
 
+
+/*
+ * Sobeluv filtr pro smer Y.
+ */
 int sobelYKernel[3][3] = {
         {-1, -2, -1},
         {0,  0,  0},
--- filtering.h
+++ filtering.h
@@ -8,7 +8,23 @@
 #include "pgm.h"
 
 /*
+ * +---+---...
+ * |abc|
+ * |def|         +---+
+ * |ghi|         |123|
+ * +---+         |456| ==> G = a*1 + b*2 + c*3 + d*4 + e*5 + f*6 + h*7 + g*9
+ * |             |789|
+ * .             +---+
+ * .
  *
+ *
+ * @brief Vypocita konvoluci obrazku se Sobelovym operatorem.
+ * @param    img            vstupni obrazek
+ * @param   mode[x, y, f]   rezim vypoctu, ovlivni vystupni obrazek
+ *              x - vystupem je aproximace derivace ve smeru X - ( G_x() + 1020 ) / 8
+ *              y - vystupem je aproximace derivace ve smeru Y - ( G_y() + 1020 ) / 8
+ *              f - vystupem je aproximace gradientu - sqrt( G_y()^2 + G_y()^2 )
+ * @return vysledek konvoluce
  */
 PGMImage *sobelFilter(PGMImage *img, char mode);
 
